3375e676c2335efc896f79d897ea792fb0cab908
Apirak Fakin <apirakfakin@gmail.com>
2025-01-21 20:24:33 +0700

    refactor: add backend parts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <main>
       <TopMenu />
-      <div className="flex flex-col place-items-center">
+      <div className="flex flex-col place-items-center py-8 gap-8">
         {items.map((item, index) => (
           <TimerOOP
             key={index}
--- frontend/src/components/TopMenu.tsx
+++ frontend/src/components/TopMenu.tsx
@@ -2,7 +2,7 @@ import MenuButton from "./MenuButton";
 
 const TopMenu = () => {
   return (
-    <div className="flex justify-end items-center relative h-10 p-4 border-4 gap-2">
+    <div className="flex justify-end items-center relative h-10 p-4 bg-slate-600 gap-2">
       <h4>Username</h4>
       <h4>MoreNames</h4>
       <MenuButton />
