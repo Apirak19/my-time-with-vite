3296ca23078f3bff9169ed8e62e90837e4327dce
Apirak Fakin <apirakfakin@gmail.com>
2025-01-27 23:40:24 +0700

    feat: add link to the TopMenu name

--- frontend/src/components/TopMenu.tsx
+++ frontend/src/components/TopMenu.tsx
@@ -3,8 +3,10 @@ import MenuButton from "./MenuButton";
 const TopMenu = () => {
   return (
     <div className="flex justify-end items-center relative h-10 p-4 bg-slate-600 gap-2">
-      <h4>Username</h4>
-      <h4>MoreNames</h4>
+      <a className="flex gap-2 text-white" href="/profile">
+        <h4>Username</h4>
+        <h4>MoreNames</h4>
+      </a>
       <MenuButton />
     </div>
   );
