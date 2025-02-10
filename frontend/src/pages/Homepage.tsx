98c1627d8719f06ded613360c2a5c15527a01fce
Apirak Fakin <apirakfakin@gmail.com>
2025-02-10 23:04:15 +0700

    style: dashboard arrangement

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
   const navigate = useNavigate();
   // fetched timers
   const [timers, setTimers] = useState<TimerProps[]>([]);
-  const [timeTypeCard, setTimeTypeCard] = useState()
+  const [timeTypeCard, setTimeTypeCard] = useState();
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   useEffect(() => {
@@ -36,20 +36,36 @@ export default function HomePage() {
   return (
     <main className="flex flex-col items-center h-screen gap-4">
       <h2 className="text-2xl">What have you done?</h2>
-      <article className="bg-slate-600 rounded-md px-4 py-3">
-        <div className="flex gap-8 items-center">
-          <div className="flex flex-col ">
-            <h4 className="text-start text-slate-300">Working</h4>
-            <h3 className="font-semibold text-2xl text-start">
-              54 <span className="font-normal text-lg">hours</span>
-            </h3>
-          </div>
+      <div className="flex gap-4">
+        <article className="bg-slate-600 rounded-md px-4 py-3">
+          <div className="flex gap-8 items-center">
+            <div className="flex flex-col ">
+              <h4 className="text-start text-slate-300">Working</h4>
+              <h3 className="font-semibold text-2xl text-start">
+                54 <span className="font-normal text-lg">hours</span>
+              </h3>
+            </div>
 
-          <div className="text-lg w-12 h-12 p-6 border-4 flex items-center justify-center rounded-full">
-            50%
+            <div className="text-lg w-12 h-12 p-6 border-4 flex items-center justify-center rounded-full">
+              50%
+            </div>
           </div>
-        </div>
-      </article>
+        </article>
+        <article className="bg-slate-600 rounded-md px-4 py-3">
+          <div className="flex gap-8 items-center">
+            <div className="flex flex-col ">
+              <h4 className="text-start text-slate-300">Working</h4>
+              <h3 className="font-semibold text-2xl text-start">
+                54 <span className="font-normal text-lg">hours</span>
+              </h3>
+            </div>
+
+            <div className="text-lg w-12 h-12 p-6 border-4 flex items-center justify-center rounded-full">
+              50%
+            </div>
+          </div>
+        </article>
+      </div>
 
       <div className="flex flex-col place-items-center py-8 gap-8">
         {items.map((item, index) => (
