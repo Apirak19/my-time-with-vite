aa627aa6b7d940be2152905731e2b9ab83e97e07
Apirak Fakin <apirakfakin@gmail.com>
2025-03-11 23:25:25 +0700

    feat: add mostTimeSpendingTasks state to store fetched data

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -13,6 +13,7 @@ export default function HomePage() {
   const navigate = useNavigate();
   // fetched timers
   const [timers, setTimers] = useState<TimerProps[]>([]);
+  const [mostTimeSpendingTasks, setMostTimeSpendingTasks] = useState<[]>([]);
   const [dashboardTotalTimeCard, setDashboardTotalTimeCard] = useState<
     DashboardTotalTimeCardType[]
   >([]);
@@ -61,58 +62,53 @@ export default function HomePage() {
 
   return (
     <main className="flex flex-col items-center h-screen gap-4">
-      {/* TotalReport */}
-      <h2 className="text-2xl">What have you done?</h2>
-      <div className="flex gap-4 overflow-x-auto whitespace-nowrap w-full max-w-[90vw] p-2">
-        {dashboardTotalTimeCard.map((item, index) => (
-          <DashboardTotalTimeCard
-            key={index}
-            cardName={item.cardName}
-            totalDuration={item.duration}
-            percentage={item.percentage}
-          />
-        ))}
-      </div>
-      {/* mostTimeSpend */}
-      <h2 className="text-2xl">What you spend doing the most?</h2>
-      <div className="flex flex-col gap-4 overflow-y-scroll whitespace-nowrap max-h-[200px] w-full max-w-[500px] p-2 scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200">
-        <div className="flex gap-2 justify-center items-center bg-slate-600 rounded-md px-4 py-3">
-          <p className="text-xl">Afternoon gaming</p>
-          <p className="text-3xl">8</p>
-          <p className="text-3xl">hours</p>
-        </div>
-        <div className="flex gap-2 justify-center items-center bg-slate-600 rounded-md px-4 py-3">
-          <p className="text-xl">Afternoon gaming</p>
-          <p className="text-3xl">8</p>
-          <p className="text-3xl">hours</p>
-        </div>
-        <div className="flex gap-2 justify-center items-center bg-slate-600 rounded-md px-4 py-3">
-          <p className="text-xl">Afternoon gaming</p>
-          <p className="text-3xl">8</p>
-          <p className="text-3xl">hours</p>
-        </div>
-        <div className="flex gap-2 justify-center items-center bg-slate-600 rounded-md px-4 py-3">
-          <p className="text-xl">Afternoon gaming</p>
-          <p className="text-3xl">8</p>
-          <p className="text-3xl">hours</p>
-        </div>
-        <div className="flex gap-2 justify-center items-center bg-slate-600 rounded-md px-4 py-3">
-          <p className="text-xl">Afternoon gaming</p>
-          <p className="text-3xl">8</p>
-          <p className="text-3xl">hours</p>
-        </div>
-      </div>
-
-      <div className="flex flex-col place-items-center py-8 gap-8">
-        {items.map((item, index) => (
-          <TimerOOP
-            key={index}
-            id={item.id}
-            timerName={item.timerName}
-            timerType={item.timerType}
-          />
-        ))}
-      </div>
+      {loading ? (
+        <div className="text-xl font-semibold mt-10">Loading...</div>
+      ) : error ? (
+        <div className="text-red-500 font-semibold mt-10">Error: {error}</div>
+      ) : (
+        <>
+          {/* TotalReport */}
+          <h2 className="text-2xl">What have you done?</h2>
+          <div className="flex gap-4 overflow-x-auto whitespace-nowrap w-full max-w-[90vw] p-2">
+            {dashboardTotalTimeCard.map((item, index) => (
+              <DashboardTotalTimeCard
+                key={index}
+                cardName={item.cardName}
+                totalDuration={item.duration}
+                percentage={item.percentage}
+              />
+            ))}
+          </div>
+  
+          {/* mostTimeSpend */}
+          <h2 className="text-2xl">What you spend doing the most?</h2>
+          <div className="flex flex-col gap-4 overflow-y-scroll whitespace-nowrap max-h-[200px] w-full max-w-[500px] p-2 scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200">
+            {/* Map over mostTimeSpendings */}
+            {mostTimeSpendingTasks.map((timer, index) => (
+              <div
+                key={index}
+                className="flex gap-2 justify-center items-center bg-slate-600 rounded-md px-4 py-3"
+              >
+                <p className="text-xl">{timer.timerName}</p>
+                <p className="text-3xl">{timer.durationHours}</p>
+                <p className="text-3xl">hours</p>
+              </div>
+            ))}
+          </div>
+  
+          <div className="flex flex-col place-items-center py-8 gap-8">
+            {items.map((item, index) => (
+              <TimerOOP
+                key={index}
+                id={item.id}
+                timerName={item.timerName}
+                timerType={item.timerType}
+              />
+            ))}
+          </div>
+        </>
+      )}
     </main>
   );
 }
