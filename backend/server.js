f6e2e3548a6ae01655c3072f8b16146b21ad211e
Apirak Fakin <apirakfakin@gmail.com>
2025-03-14 00:42:16 +0700

    feat: add queries to get Time spent per day, week, and month

--- backend/server.js
+++ backend/server.js
@@ -64,15 +64,64 @@ app.get("/getStat", async (req, res) => {
       {
         $project: {
           _id: 0,
-          timerType: "$_id", // Renaming _id to timerType
-          totalDuration: 1, // Keeping totalDuration as a number
+          timerType: "$_id",
+          totalDuration: 1,
         },
       },
     ]);
+
     const mostSpend = await timerModel.find().sort({ durationHours: -1 });
+
+    // Time spent per day, week, and month
+    const timeSpentByPeriod = await timerModel.aggregate([
+      {
+        $group: {
+          _id: {
+            day: { $dateToString: { format: "%Y-%m-%d", date: "$startTime" } },
+            week: { $isoWeek: "$startTime" },
+            month: { $dateToString: { format: "%Y-%m", date: "$startTime" } },
+          },
+          totalDuration: { $sum: "$durationHours" },
+        },
+      },
+    ]);
+
+    // Peak productivity hours
+    const peakHours = await timerModel.aggregate([
+      {
+        $group: {
+          _id: { $hour: "$startTime" }, // Grouping by hour of the day
+          totalDuration: { $sum: "$durationHours" },
+        },
+      },
+      { $sort: { totalDuration: -1 } }, // Sorting to find peak hours
+    ]);
+
+    // Idle vs. Active time
+    const idleVsActive = await timerModel.aggregate([
+      {
+        $group: {
+          _id: null,
+          totalActiveTime: { $sum: "$durationHours" },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          totalActiveTime: 1,
+          totalIdleTime: {
+            $subtract: [24, { $sum: "$durationHours" }], // Assuming a max of 24h per day
+          },
+        },
+      },
+    ]);
+
     res.status(200).json({
-      totalByTimerType: totalByTimerType,
-      mostSpend: mostSpend,
+      totalByTimerType,
+      mostSpend,
+      timeSpentByPeriod,
+      peakHours,
+      idleVsActive,
     });
   } catch {
     console.error("error occured");
